38b212ec838811d4fe4481ef038d69cb5bc43dd8
Jose Galvis <jagn1997@hotmail.com>
2022-03-25 15:13:27 +0100

    [fix] twitter gateway

--- src/gateways/twitterGateway/twitterGateway.js
+++ src/gateways/twitterGateway/twitterGateway.js
@@ -2,9 +2,13 @@ const twitterRestClient = require('./twitterRestClient');
 
 async function getTimeline(screen_name, count) {
     const path = `/statuses/user_timeline.json?screen_name=${screen_name}&count=${count}`;
-    const response = await twitterRestClient.makeRequest(path, 'GET');
-
-    return response.data;
+    try {
+        const response = await twitterRestClient.makeRequest(path, 'GET');
+        return response.data;
+    } catch (error) {
+        return [];
+    }
+    
 }
 
 module.exports = {
